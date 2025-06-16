33f83ca20fbd12a4da81f0eed40729721002ce99
SanaHameed2 <sana.faiza2@gmail.com>
2025-06-16 09:44:09 +0500

    changes made i n js

--- Javascript-arrays/app.js
+++ Javascript-arrays/app.js
@@ -2,7 +2,7 @@
 // Topic: Array Creation & Indexing
 
 let fruits = ["Orange", "Banana", "Cherry", "Peach", "Mango"];
-console.log(fruits[0]); // Apple
+console.log(fruits[0]); // Orange
 console.log(fruits[1]); // Banana
 console.log(fruits[2]); // Cherry
 console.log(fruits[3]); // Peach
@@ -16,8 +16,19 @@ fruits.push("Orange"); // Adds "Orange" to the end of the array
 console.log(fruits); // ["Apple", "Banana", "Cherry", "Peach", "Mango", "Orange"]
 
 fruits.pop(); // Removes the last item ("Orange")
-console.log(fruits); // ["Apple", "Banana", "Cherry", "Peach", "Mango"]
+console.log(fruits); 
 
 // Purpose: Add and remove items using unshift() and shift()
 // Topic: Array Methods - unshift & shift
 
+let fruits = ["Apple", "Banana", "Cherry", "Peach", "Mango"];
+fruits.unshift("Orage"); // Adds "Orange" to the beginning of the array
+console.log(fruits);
+
+
+// // Purpose: Add and remove items using unshift() and shift()
+// Topic: Array Methods - unshift & shift
+
+let fruits = ["Apple", "Banana", "Cherry", "Peach", "Mango"];
+fruits.shift(); // Removes the first item ("Orange")
+console.log(fruits); // ["Banana", "Cherry", "Peach", "Mango"]
